302362fb645e9a54e7b201f63541a5792a9b874d
mitzimorris <mitzi@panix.com>
2013-12-05 15:41:23 -0500

    cleanup

--- src/io/src/com/colloquial/io/CopyFile.java
+++ src/io/src/com/colloquial/io/CopyFile.java
@@ -1,7 +1,5 @@
 package com.colloquial.io;
 
-import com.aliasi.util.Streams;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -29,34 +27,5 @@ public class CopyFile {
         /*x*/
     }
 
-    public static void main2(String[] args)
-        throws FileNotFoundException, IOException {
-
-        File fileIn = new File(args[0]);
-        File fileOut = new File(args[1]);
-        /*x CopyFile.2 */
-        InputStream in = null;
-        try {
-            in = new FileInputStream(fileIn);
-            OutputStream out = null;
-            try {
-                out = new FileOutputStream(fileOut);
-                byte[] buf = new byte[8192];
-                int n;
-                try {
-                    while ((n = in.read(buf)) >= 0)
-                        out.write(buf,0,n);
-                } catch (IOException e) {
-                    fileOut.delete();
-                    throw e;
-                }
-            } finally {
-                Streams.closeQuietly(out);
-            }
-        } finally {
-            Streams.closeQuietly(in);
-        }
-        /*x*/
-    }
 
 }
