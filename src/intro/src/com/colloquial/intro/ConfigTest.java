610809fbd178d704948be6b9820a02e06dee789d
mitzimorris <mitzi@panix.com>
2013-12-05 14:54:49 -0500

    buildfile cleanup

--- src/intro/src/com/colloquial/intro/ConfigTest.java
+++ src/intro/src/com/colloquial/intro/ConfigTest.java
@@ -1,7 +1,9 @@
 package com.colloquial.intro;
+import java.io.PrintStream;
 public class ConfigTest {
     public static void main(String[] args) throws Exception {
         /*x ConfigTest.1*/
+        System.setOut(new PrintStream(System.out,true,"UTF-8"));
         byte[] bs = "small a: a\n".getBytes("UTF-8");
         System.out.write(bs,0,bs.length);
         bs = "small a with grave accent: \u00E0\n".getBytes("UTF-8");
